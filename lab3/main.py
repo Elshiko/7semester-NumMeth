c9bd3a32f2bb66f019a57a2fab0f979d779ebce0
Elshiko <Minak.S@mail.ru>
2014-12-15 01:11:55 +0400

    now it works

--- lab3/main.py
+++ lab3/main.py
@@ -3,25 +3,23 @@ import math
 import sys
 import miln
 import matplotlib.pyplot as pplot
+from math import *
 
 def show_help():
   print('help --> show this message\n'
         'func=\'EXPR\' --> equation, argument x must be $x in input, y must be $y, func = y\'\n'
         'condition=x%y, where x = x0, y = y(x0)\n'
         'interval=A%B --> interval of solving [A;B]\n'
-        'graph=C --> y - enable charts, n - disable\n'
-        'silent=y/n --> enable/disable silent mode, :::All char except \'y\' = \'n\'\n'
         'answer=\'EXPR\' --> function y(x), x -> $x\n'
-        'chart=TYPE --> set chart type DELTA or BOTH, DELTA_PERCENT, SOLVE !!!if there is no answer, than will be SOLVE chart\n'
-        'precision=Z --> eps = 1e-Z')
+        'step=Z --> set H')
 
 def calculate(func, x):
   """calculate y=f(x)"""
   func = func.replace('$x', str(x))
   return eval(func)
 
-exist_args = ['func', 'condition', 'interval', 'graph', 'answer', 'silent', 'chart', 'precision']
-params = {'func' : '$x', 'condition' : '0%0', 'interval' : '0%1', 'graph' : 'y', 'answer' : '$x*$x/2', 'silent' : 'n', 'chart' : 'BOTH', 'precision' : '4'}
+exist_args = ['func', 'condition', 'interval', 'answer', 'step']
+params = {'func' : '$x', 'condition' : '0%0', 'interval' : '0%1', 'answer' : '$x*$x/2', 'step' : '0.05', 'is_answer' : False}
 
 
 for raw_arg in sys.argv:
@@ -35,6 +33,8 @@ for raw_arg in sys.argv:
     print('Incorrect parameter \'' + raw_arg + '\'')
   else:
     params[arg[0]] = arg[1]
+    if arg[0] == 'answer':
+      params['is_answer'] = True
 
 params['condition'] = params['condition'].split('%')
 if len(params['condition']) != 2:
@@ -51,20 +51,40 @@ params['interval'][1] = float(params['interval'][1])
 if params['interval'][0] > params['interval'][1] or params['condition'][0] < params['interval'][0] or params['condition'][0] > params['interval'][1]:
   print('Incorrect interval or condition')
   exit(1)
-if params['graph'] == 'y':
-  params['graph'] = True
-else:
-  params['graph'] = False
-if params['silent'] == 'y':
-  params['silent'] = True
-else:
-  params['silent'] = False
-params['precision'] = 10 ** (-1*int(params['precision']))
+params['step'] = float(params['step'])
 
-#x_rank2, y_rank2 = miln.use_rank_two(params['func'], params['condition'], params['interval'], params['precision'])
-#x_rank3, y_rank3 = miln.use_rank_three(params['func'], params['condition'], params['interval'], params['precision'])
-x_rank4, y_rank4 = miln.use_rank_four(params['func'], params['condition'], params['interval'], params['precision'])
-#x_rank5, y_rank5 = miln.use_rank_five(params['func'], params['condition'], params['interval'], params['precision'])
-zp = zip(x_rank4, y_rank4)
-pr = lambda a: print(str(a[0]) + '     :     ' + str(a[1]) + '     -      ' + str(calculate(params['answer'], a[0])))
-ans = list(map(pr, zp))
+x_rank2, y_rank2 = miln.solve(params['func'], params['condition'], params['interval'], params['step'], 2)
+x_rank3, y_rank3 = miln.solve(params['func'], params['condition'], params['interval'], params['step'], 3)
+x_rank4, y_rank4 = miln.solve(params['func'], params['condition'], params['interval'], params['step'], 4)
+x_rank5, y_rank5 = miln.solve(params['func'], params['condition'], params['interval'], params['step'], 5)
+x = [x_rank2, x_rank3, x_rank4, x_rank5]
+y = [y_rank2, y_rank3, y_rank4, y_rank5]
+
+dlt = lambda a, b: abs(a - b)
+pplot.figure(1)
+in_col = 1
+in_row = 4
+if params['is_answer']:
+  in_row += 1
+  in_col += 1
+for i in range(1, 5):
+  pplot.subplot(in_col, in_row, i)
+  pplot.plot(x[i - 1], y[i - 1])
+  pplot.xlabel('x')
+  pplot.ylabel('u rank ' + str(i + 1))
+if params['is_answer']:
+  real = list(map(lambda a: calculate(params['answer'], a), x[0]))
+  pplot.subplot(in_col, in_row, 5)
+  pplot.plot(x[0], real)
+  pplot.xlabel('x')
+  pplot.ylabel('real function')
+  for i in range(6, 10):
+    pplot.subplot(in_col, in_row, i)
+    pplot.plot(x[i - 6], list(map(dlt, y[i - 6], real)))
+    pplot.xlabel('x')
+    pplot.ylabel('abs(y_rank_' + str(i + 1) + ' - real function)')
+  pplot.subplot(in_col, in_row, 10)
+  pplot.plot(x[0], y[0], 'bs', x[1], y[1], 'gs', x[2], y[2], 'rs', x[3], y[3], 'g^')
+  pplot.xlabel('x')
+  pplot.ylabel('abs(y - real)')
+pplot.show()
--- lab3/miln.py
+++ lab3/miln.py
@@ -1,18 +1,14 @@
+from math import *
 def calculate(func, x, y):
   """calculate f(x, y)"""
   func = func.replace('$x', str(x))
   func = func.replace('$y', str(y))
   return eval(func)
 
-def use_rank_two(func, cond, interval, precision):
-  pass
-
-def use_rank_three(func, cond, interval, precision):
-  pass
-
-def use_rank_four(func, cond, interval, precision):
+def solve(func, cond, interval, h, rank):
+  """y[i] = y[i-2] + 2 * h * f(x[i-1], y[i-1])
+     y[i] = y[i-3] + 3*h/4*(-3*f[i-3]+8*f[i-2]-f[i-1])"""
   eps = 1e-6
-  h = 0.01
   left_bound = interval[0]
   right_bound = interval[1]
   x0 = cond[0]
@@ -22,16 +18,30 @@ def use_rank_four(func, cond, interval, precision):
   if left_bound < x0:
     x_list.append(x0)
     y_list.append(y0)
-    for step in range(3):
+    for step in range(rank - 1):
       if x_list[-1] - h < left_bound + eps:
         break
       x_list.append(x_list[-1] - h)
-      y_list.append(y_list[-1] + h*calculate(func, x_list[-1], y_list[-1]))
+      y_list.append(y_list[-1] - h*calculate(func, x_list[-1], y_list[-1]))
     while x_list[-1] - h > left_bound - eps:
-      first_y = y_list[-4] + 4*h*(2*calculate(func, x_list[-1], y_list[-1])
-                                  -calculate(func, x_list[-2], y_list[-2])
-                                  +2*calculate(func, x_list[-3], y_list[-3]))/3
-      real_y = y_list[-2] + h*(calculate(func, x_list[-1] + h, first_y)
+      first_y = 0
+      if rank == 2:
+        first_y = y_list[-2] - 2*h*calculate(func, x_list[-1], y_list[-1])
+      elif rank == 3:
+        first_y = y_list[-3] - 3*h*(-3*calculate(func, x_list[-3], y_list[-3])
+                                    +8*calculate(func, x_list[-2], y_list[-2])
+                                    -calculate(func, x_list[-1], y_list[-1]))/4
+      elif rank == 4:
+        first_y = y_list[-4] - 4*h*(2*calculate(func, x_list[-1], y_list[-1])
+                                    -calculate(func, x_list[-2], y_list[-2])
+                                    +2*calculate(func, x_list[-3], y_list[-3]))/3
+      else:
+        first_y = y_list[-5] - 25*h*(193*calculate(func, x_list[-5], y_list[-5])
+                                     -550*calculate(func, x_list[-4], y_list[-4])
+                                     +648*calculate(func, x_list[-3], y_list[-3])
+                                     -454*calculate(func, x_list[-2], y_list[-2])
+                                     +127*calculate(func, x_list[-1], y_list[-1]))/144
+      real_y = y_list[-2] - h*(calculate(func, x_list[-1] + h, first_y)
                                +4*calculate(func, x_list[-1], y_list[-1])
                                +calculate(func, x_list[-2], y_list[-2]))/3
       x_list.append(x_list[-1] - h)
@@ -40,23 +50,31 @@ def use_rank_four(func, cond, interval, precision):
     y_list = y_list[:0:-1]
   x_list.append(x0)
   y_list.append(y0)
-  for step in range(3):
+  for step in range(rank - 1):
     if x_list[-1] + h > right_bound + eps:
       break
     y_list.append(y_list[-1] + h*calculate(func, x_list[-1], y_list[-1]))
     x_list.append(x_list[-1] + h)
   while x_list[-1] + h <= right_bound + eps:
-    first_y = y_list[-4] + 4*h*(2*calculate(func, x_list[-1], y_list[-1])
-                                -calculate(func, x_list[-2], y_list[-2])
-                                +2*calculate(func, x_list[-3], y_list[-3]))/3
+    if rank == 2:
+      first_y = y_list[-2] + 2*h*calculate(func, x_list[-1], y_list[-1])
+    elif rank == 3:
+      first_y = y_list[-3] + 3*h*(-3*calculate(func, x_list[-3], y_list[-3])
+                                  +8*calculate(func, x_list[-2], y_list[-2])
+                                  -calculate(func, x_list[-1], y_list[-1]))/4
+    elif rank == 4:
+      first_y = y_list[-4] + 4*h*(2*calculate(func, x_list[-1], y_list[-1])
+                                  -calculate(func, x_list[-2], y_list[-2])
+                                  +2*calculate(func, x_list[-3], y_list[-3]))/3
+    else:
+        first_y = y_list[-5] + 25*h*(193*calculate(func, x_list[-5], y_list[-5])
+                                     -550*calculate(func, x_list[-4], y_list[-4])
+                                     +648*calculate(func, x_list[-3], y_list[-3])
+                                     -454*calculate(func, x_list[-2], y_list[-2])
+                                     +127*calculate(func, x_list[-1], y_list[-1]))/144
     real_y = y_list[-2] + h*(calculate(func, x_list[-1] + h, first_y)
                              +4*calculate(func, x_list[-1], y_list[-1])
                              +calculate(func, x_list[-2], y_list[-2]))/3
     x_list.append(x_list[-1] + h)
     y_list.append(real_y)
-
-
   return x_list, y_list
-
-def use_rank_five(func, cond, interval, precision):
-  pass
